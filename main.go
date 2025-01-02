da34657a03a903f28a81cbd7e202a799bb388ce7
AnanyaSinha2717 <ananyasinha2717@gmail.com>
2025-01-02 23:27:28 +0530

    add http methods-WIP

--- main.go
+++ main.go
@@ -1,10 +1,47 @@
 package main
 
 import (
-	"log"
 	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 )
 
-func main() {
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 
+	w.Write([]byte("Hello from Bug Tracker\n"))
+}
+
+func bugView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display bug with ID %d...", id)
+}
+
+func bugCreate(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "POST" {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Write([]byte("Create a new bug\n"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/bug/view/", bugView)
+	mux.HandleFunc("/bug/create/", bugCreate)
+
+	log.Println("Starting server on: 4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
