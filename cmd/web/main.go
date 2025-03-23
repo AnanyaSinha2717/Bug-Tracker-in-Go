bc6fc4bc49490df81c283ce1bb4ac696ede0fdf4
AnanyaSinha2717 <ananyasinha2717@gmail.com>
2025-03-23 13:53:34 +0530

    add command line flag

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// cmd line flag, completely optional
+	addr := flag.String("addr", ":4000", "HTTP Network Address") // this is a pointer which we deref  later
+
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	// fileServer := http.FileServer(http.Dir("./ui/html/base.tmpl.html/"))
 	// mux.Handle("/base.tmpl.html/", http.StripPrefix("/base.tmpl.html", fileServer))
@@ -13,7 +19,7 @@ func main() {
 	mux.HandleFunc("/bug/view/", bugView)
 	mux.HandleFunc("/bug/create/", bugCreate) 
 
-	log.Println("Starting server on: 4000")
+	log.Printf("Starting server on: %s", *addr)
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
