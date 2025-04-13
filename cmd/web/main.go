023b1efbcaf85a9f0f24cfae5672dbe06075e1ac
AnanyaSinha2717 <ananyasinha2717@gmail.com>
2025-04-13 20:55:40 +0530

    create database pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -15,11 +17,19 @@ type application struct {
 func main() {
 	// cmd line flag, completely optional
 	addr := flag.String("addr", ":4000", "HTTP Network Address") // this is a pointer which we deref  later
+	dsn := flag.String("dsn", "lily:BOSS@/buggo?parseTime=true", "MySQL data source name")
 
 	flag.Parse()
 
 	infolog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errlog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err:= openDB(*dsn)
+	if err != nil {
+		errlog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		errlog: errlog,
@@ -34,6 +44,19 @@ func main() {
 	}
 
 	infolog.Printf("Starting server on: %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errlog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := mysql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
